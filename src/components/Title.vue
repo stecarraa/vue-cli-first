45941a40b37827424872007842875b173892ca1d
stecarraa <stefano@caratozzolo.me>
2022-07-14 14:21:14 +0200

    feat: exercise ok

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTitle',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
