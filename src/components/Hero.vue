cd9e982785ac2af7cd4d5b0d0d7437d13a604471
stecarraa <stefano@caratozzolo.me>
2022-07-14 14:59:12 +0200

    feat : triing to install bootstrap

---
+++ src/components/Hero.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+   <div class="ms-container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="">Quante cose si possssono fare</h1>
+        </div>
+    </div>
+
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JumbotronHero',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.ms-container{
+    background-image: url('/Users/stecarraa/Documents/Boolean-Repo/vue-cli-first/src/assets/86738.jpeg') ;
+    height: 100vh;
+    background-size: contain;
+}
+</style>
